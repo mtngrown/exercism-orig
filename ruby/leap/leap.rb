64fd748aefadf2ed5ab5a392a7b093c66ab67581
Juan Valdez <mtngrown7@yahoo.com>
2017-04-22 10:58:21 -0700

    first commit

---
+++ ruby/leap/leap.rb
@@ -0,0 +1,19 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Year
+  class << self
+    def leap? year
+      mod_zero?(year, 400) || non_century?(year)
+    end
+
+    def non_century? year
+      mod_zero?(year, 4) && !mod_zero?(year, 100)
+    end
+
+    def mod_zero? year, divisor
+      (year % divisor).zero?
+    end
+  end
+end
